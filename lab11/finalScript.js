7b71aa58bf596c5d6e160dbc33ac1765b61bab8a
lamtruong001 <lam.truong96@gmail.com>
2018-03-08 09:09:16 +0000

    changes

--- lab11/finalScript.js
+++ lab11/finalScript.js
@@ -1,10 +1,10 @@
 $(function(){
 //document ready
-//alert("document ready");
+alert("document ready");
 
   $('#searchform').submit(function(){
-    var serachterms = $ ("#searchterms").val();
-    getResultsFromOMDB (searchterms);
+    var serachterms = $("#searchterms").val();
+    getResultsFromOMDB(searchterms);
     //addItemToList(searchterms);
     return false;
   });
@@ -15,17 +15,15 @@ function getResultsFromOMDB(searchterms) {
   var url = "http://www.omdbapi.com/?i=tt3896198&apikey=86a5f8a4&s=" + searchterms;
   $.getJSON(url, function(jsondata){
     addResultTitles(jsondata);
-
   });
 }
 
 function addResultTitles(jsondata) {
   var htmlstring = "";
-for (var i=0; i<10; i++) {
-  var title = jsondata.Search[i].Title;
-  htmlstring += "<li>"+ title + "</li>";
+  for (var i=0; i<10; i++) {
+    var title = jsondata.Search[i].Title;
+    htmlstring += "<li>"+ title + "</li>";
   }
 
   $("results").html(htmlstring);
-
-  }
+}
