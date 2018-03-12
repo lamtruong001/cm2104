005ed627dcfaa3263a69f0295ae04d2adbab162f
lamtruong001 <lam.truong96@gmail.com>
2018-03-12 17:50:43 +0000

    change

--- lab13/mysecondserver.js
+++ lab13/mysecondserver.js
@@ -3,6 +3,6 @@ var currentdate = require('./mymodule');
 
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
- res.write('The date and time are currently:' + currentdate.myDateTime);
+ res.write("The date and time are currently: " + currentdate.myDateTime);
  res.end('Hello World');
 }).listen(8080);
