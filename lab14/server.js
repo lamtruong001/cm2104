0bb4b95386d4401cc6b34e9df566e75299af3177
1605199 <lam.truong96>
2018-03-15 11:11:24 +0000

    changes n2

--- lab14/server.js
+++ lab14/server.js
@@ -1,7 +1,15 @@
-var express = require("express");
+var express = require('express');
 var app = express();
- app.use(express.static("public"))
- app.get("/", function(req, res){
+var Twitter = require('twitter');
+var client = new Twitter({
+
+ consumer_key: 'bqbudmaviN9Si55v1zR4sWo6c',
+ consumer_secret: 'SFffqBoj8NikupjUl7oE95o6RUVhQRkJLJ9qduvF8v98zN05wb',
+ access_token_key: '578384873-1gZKSQhujTrisUwuUwXr8efwNbMaO8ugWP11rg8Y',
+ access_token_secret: 'PJD6YgPKuQLAVvBUQbIvWfL3GL5VrmmfbM8628ESSppBh'
+});
+app.use(express.static('public'))
+app.get('/', function(req, res){
  res.send("Hello world! by express");
 });
 app.listen(8080);
