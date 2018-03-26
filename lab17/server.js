2e59498b4b4c4d06b3a3ea1c9e000cd52f1e87f3
lamtruong001 <lam.truong96@gmail.com>
2018-03-26 15:26:36 +0100

    change

--- lab17/server.js
+++ lab17/server.js
@@ -6,6 +6,24 @@ var app = express();
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 // index page
+
+app.get('/', function(req, res) {
+ var drinks = [
+ { name: 'Bloody Mary', drunkness: 3 },
+ { name: 'Martini', drunkness: 5 },
+ { name: 'Scotch', drunkness: 10 }
+ ];
+ var tagline = "Any code of your own that
+you haven't looked at for six or more months
+might as well have been written by someone
+else.";
+ res.render('pages/index', {
+ drinks: drinks,
+ tagline: tagline
+ });
+});
+
+
 // app.get('/', function(req, res) {
 //  res.render('pages/index');
 // });
@@ -22,18 +40,3 @@ console.log('8080 is the magic port');
 // res.render('pages/index');
 // });
 // index page
-app.get('/', function(req, res) {
- var drinks = [
- { name: 'Bloody Mary', drunkness: 3 },
- { name: 'Martini', drunkness: 5 },
- { name: 'Scotch', drunkness: 10 }
- ];
- var tagline = "Any code of your own that
-you haven't looked at for six or more months
-might as well have been written by someone
-else.";
- res.render('pages/index', {
- drinks: drinks,
- tagline: tagline
- });
-});
