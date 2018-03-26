e788904e94ec5d80aacfed9e6c4083e71a87e122
lamtruong001 <lam.truong96@gmail.com>
2018-03-26 15:27:24 +0100

    change

--- lab17/server.js
+++ lab17/server.js
@@ -13,10 +13,7 @@ app.get('/', function(req, res) {
  { name: 'Martini', drunkness: 5 },
  { name: 'Scotch', drunkness: 10 }
  ];
- var tagline = "Any code of your own that
-you haven't looked at for six or more months
-might as well have been written by someone
-else.";
+ var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
  res.render('pages/index', {
  drinks: drinks,
  tagline: tagline
